472decf2166b949afe427b132ca3374ff23ea88f
python-dev-bsram <bsram1625@gmail.com>
2024-11-08 15:42:49 +0530

    Update unit test case test_dependency_override.py

--- fastapi-pytest/tests/test_dependency_override.py
+++ fastapi-pytest/tests/test_dependency_override.py
@@ -1,11 +1,28 @@
 from app.main import app, fake_authentication
+from fastapi import HTTPException, status
+import pytest
 
-def mock_authentication(token: str):
-    return ""
+
+# pass token when invalid and test using mock
+async def mock_authentication(token: str):
+    if token == "invalid-token":
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token"
+        )
+    return True
+
 
 @pytest.mark.asyncio
 async def test_protected_route_with_mock(client):
     app.dependency_overrides[fake_authentication] = mock_authentication
-    response = await client.get("/protected")
+    response = await client.get("/protected?token=valid-token")
     assert response.status_code == 200
     assert response.json() == {"message": "Authenticated!"}
+
+
+@pytest.mark.asyncio
+async def test_protected_route_with_invalid_token(client):
+    app.dependency_overrides[fake_authentication] = mock_authentication
+    response = await client.get("/protected?token=invalid-token")
+    assert response.status_code == 401
+    assert response.json() == {"detail": "Invalid token"}
