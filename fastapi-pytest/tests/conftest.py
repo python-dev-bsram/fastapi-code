f6bbf566ac3b25cf827ecce77417ad31e0e84412
python-dev-bsram <bsram1625@gmail.com>
2024-11-08 15:43:37 +0530

    Delete fastapi-pytest/tests/conftest.py

--- fastapi-pytest/tests/conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from httpx import AsyncClient
-from app.main import app
-
-@pytest.fixture
-async def client():
-    async with AsyncClient(app=app, base_url="http://test") as ac:
-        yield ac
